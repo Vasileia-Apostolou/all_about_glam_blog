aceefebff365e54dfdeefde248ba3160c9b02ab7
Vasileia-Apostolou <vasiliaapostolou@gmail.com>
2020-11-09 20:49:21 +0000

    Create base template

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 import os
-from flask import Flask 
+from flask import  (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo 
+from bson.objectid import ObjectId 
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONDO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def hello():
-    return "Hello"
+@app.route("/blog_posts")
+def blog_posts():
+    blogs = mongo.db.blogpost.find()
+    return render_template("blogpost.html", blogs=blogs)
 
 
 if __name__ == "__main__":
