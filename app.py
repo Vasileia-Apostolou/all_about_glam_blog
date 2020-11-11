768bbe1b3181af092cd70d2d1d6eb04fda48f08a
Vasileia-Apostolou <vasiliaapostolou@gmail.com>
2020-11-11 22:33:53 +0000

    Add create new post template

--- app.py
+++ app.py
@@ -90,6 +90,10 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/create_blog")
+def create_blog():
+    return render_template("create_blog.html")
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
     port=int(os.environ.get("PORT")),
