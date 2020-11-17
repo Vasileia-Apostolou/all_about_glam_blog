d11b3b148d32b009f94630b1ee9059d8f01a26b1
Vasileia-Apostolou <vasiliaapostolou@gmail.com>
2020-11-17 21:16:04 +0000

    Change Debug to False and Finish Project For Submission

--- app.py
+++ app.py
@@ -195,4 +195,4 @@ def view_post(blogpost_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
